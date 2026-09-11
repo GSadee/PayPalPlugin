c034bb5044924b582566d4967371f7dd0f0ca5ca
Grzegorz Sadowski <sadowskigp@gmail.com>
2026-09-11 13:48:50 +0200

    Cover the v6 PayPal payment page with functional and Behat tests

---
+++ tests/Behat/Page/Shop/PayWithPayPalPage.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\PayPalPlugin\Behat\Page\Shop;
+
+use Sylius\Behat\Page\SyliusPage;
+
+final class PayWithPayPalPage extends SyliusPage
+{
+    public function getRouteName(): string
+    {
+        return 'sylius_paypal_shop_pay_with_paypal_form';
+    }
+
+    public function hasPayPalButton(): bool
+    {
+        return $this->hasElement('paypal_button');
+    }
+
+    public function hasCardFields(): bool
+    {
+        return $this->hasElement('card_fields');
+    }
+
+    public function isRenderedInTheShopLayout(): bool
+    {
+        return $this->hasElement('shop_layout_body');
+    }
+
+    public function loadsLegacyPayPalSdk(): bool
+    {
+        return $this->hasElement('legacy_paypal_sdk');
+    }
+
+    protected function getDefinedElements(): array
+    {
+        return array_merge(parent::getDefinedElements(), [
+            'card_fields' => '[data-controller~="sylius--paypal-plugin--paypal-card-fields"]',
+            'legacy_paypal_sdk' => 'script[src*="paypal.com/sdk/js"]',
+            'paypal_button' => '[data-controller~="sylius--paypal-plugin--paypal-wallet-button"]',
+            'shop_layout_body' => 'body[data-route="sylius_paypal_shop_pay_with_paypal_form"]',
+        ]);
+    }
+}
